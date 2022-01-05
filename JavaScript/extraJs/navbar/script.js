4f915128486fe48f6045e7131d0fcc6702752881
vijay <Javascript_user>
2022-01-05 22:27:30 +0530

    Portpolio Part 2 updating Navbar

---
+++ JavaScript/extraJs/navbar/script.js
@@ -0,0 +1,7 @@
+const part1= document.getElementById('part1');
+const btn =document.querySelector('.btn');
+
+btn.addEventListener('click', () =>{
+    part1.style.backgroundColor="red";
+    part1.style.width= "200px";
+})
